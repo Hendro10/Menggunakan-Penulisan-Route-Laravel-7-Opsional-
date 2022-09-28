4a8552538645c770feff864ea6dbefd04ed532b1
Hendro Gunawan <112385556+Hendro10@users.noreply.github.com>
2022-09-28 19:56:51 +0700

    Add files via upload

---
+++ MahasiswaController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function mahasiswa()
+    {
+        $arrMahasiswa = ["Hendro Gunawan","Risa Lestari","Rudi Hermawan","Bambang Kusumo",
+        "Lisa Permata"];
+
+        return view('mahasiswa')->with('mahasiswa', $arrMahasiswa);
+    }
+
+    public function dosen()
+    {
+        $arrDosen = ["Maya Fitrianti, M.M.","Prof. Silvia Nst, M.Farm.",
+        "Dr. Umar Agustinus", "Dr. Syahrial, M.Kom."];
+
+        return view('dosen')->with('dosen', $arrDosen);
+    }
+
+    public function gallery()
+    {
+        return view('gallery');
+    }
+
+    public function info($fakultas, $jurusan)
+    {
+        $data = [$fakultas, $jurusan];
+        return view('informasi')->with('data', $data);
+    }
+
+}
---
+++ web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/mahasiswa', 'MahasiswaController@mahasiswa')->name('mahasiswa');
+Route::get('/dosen', 'MahasiswaController@dosen')->name('dosen');
+Route::get('/gallery', 'MahasiswaController@gallery')->name('gallery');
+Route::get('informasi/{fakultas}/{jurusan}', 'MahasiswaController@info')->name('info');
